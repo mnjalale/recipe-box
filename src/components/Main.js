b0fab84b973afaeda5d924f06205edb3c9ab954f
Martin Njalale <mnjalale@gmail.com>
2016-07-31 12:23:08 +0300

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main(props){
+    return (
+        <div>
+            This is the home page
+        </div>
+    )
+}
+
+export default Main;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry:[
+        'babel-polyfill',
+        './src/index.js'
+    ],
+    output:{
+        path: __dirname + '/dist',
+        filename: "index_bundle.js"
+    },
+    module: {
+        loaders: [
+            {test:/\.js$/,exclude:/node_modules/,loader:"babel-loader"},
+            {test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass')}
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig,
+        new ExtractTextPlugin('style.css')
+    ]
+}
